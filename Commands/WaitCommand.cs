5da8fe40593d5ddbcc89f6461cd93db02c324e4c
Nerx <nikitahysnutdinov@gmail.com>
2026-05-06 15:56:59 +0700

    Команда WAIT

---
+++ Commands/WaitCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace DeepSeekAgent.Commands;
+
+public class WaitCommand : LocalCommand
+{
+    public override string Name => "WAIT";
+
+    public override async Task<string> ExecuteAsync(string input)
+    {
+        var time = int.Parse(input);
+        await Task.Delay(time);
+
+        return """
+           RESPONSE WAIT
+           END RESPONSE
+           """;
+    }
+}
