5ad8987f9b93e7f722fef8e54b553c0d0ac45a78
Nerx <nikitahysnutdinov@gmail.com>
2026-05-06 23:12:54 +0700

    Команда перезапуска утилит

---
+++ Commands/System/RestartCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace DeepSeekAgent.Commands.System;
+
+public class RestartCommand : LocalCommand
+{
+    public override string Name => "RESTART";
+
+    public override Task<string> ExecuteAsync(string payload)
+    {
+        switch (payload)
+        {
+            case "WSL":
+                {
+                    WslManager.Stop();
+                    WslManager.StartWsl();
+                    return Task.FromResult("WSL перезапущен");
+                }
+            case "POWERSHELL":
+                {
+                    PowerShellManager.Stop();
+                    PowerShellManager.StartPowerShell();
+                    return Task.FromResult("POWERSHELL перезапущен");
+                }
+            default:
+                {
+                    return Task.FromResult($"{payload} не найден");
+                }
+        }
+    }
+}
